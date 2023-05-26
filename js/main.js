3bafc58bfd964ebdd46fbed05f0f7b6c0e4d4f24
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-05-26 13:21:48 -0300

    realizado a calculadora

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 const resultado = document.querySelector('.result');
 const confirmar = document.querySelector('.igual');
+
+function inserir(valor) {
+    resultado.innerHTML += valor;
+}
+
+function limpar() {
+    resultado.innerHTML = '';
+}
+
+function apagar() {
+    if (resultado.textContent) {
+        let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length - 1);
+    }
+}
+
+function confirma() {
+    if (resultado.textContent != 'Erro') {
+        document.getElementById("resultado").innerHTML = eval(resultado.innerHTML)
+    }
+}
